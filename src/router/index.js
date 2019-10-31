ea1f01a79943f7352adfc035782b5c20ad078dca
likole <likolef@gmail.com>
2019-10-31 11:15:01 +0800

    用户身份区分

--- src/router/index.js
+++ src/router/index.js
@@ -152,6 +152,20 @@ export const constantRoutes = [
         meta: { title: '积分记录', icon: 'list', noCache: true }
       }
     ]
+  },
+  {
+    path: '/profile',
+    component: Layout,
+    redirect: '/profile/index',
+    hidden: true,
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/profile/index'),
+        name: 'Profile',
+        meta: { title: '个人中心', icon: 'user', noCache: true }
+      }
+    ]
   }
 
 ]
