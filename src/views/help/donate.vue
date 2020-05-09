c9baa041fac858beac701ffd5048dd93dae55ff2
likole <likolef@gmail.com>
2020-05-09 18:05:05 +0800

    微调代码格式

--- src/views/help/donate.vue
+++ src/views/help/donate.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="app-container">
     <el-card class="box-card" style="margin-bottom: 20px">
-      <p>如果您觉得 IMUDGESOJ 所提供的服务给您带来了帮助，您可以考虑扫描下方的{{pay?'支付宝收款码':'红包码'}}，来继续鼓励支持我~</p>
+      <p>如果您觉得 IMUDGESOJ 所提供的服务给您带来了帮助，您可以考虑扫描下方的{{ pay?'支付宝收款码':'红包码' }}，来继续鼓励支持我~</p>
       <div align="center">
         <img v-if="pay" src="@/assets/alipay.png" height="205px">
         <img v-else src="@/assets/red_envelope.png" height="200px">
-        <br/>
+        <br>
         <el-button type="text" @click="pay=!pay">{{ pay?'切换为红包码':'土豪可点击此处切换为 支付宝收款码' }}</el-button>
       </div>
     </el-card>
