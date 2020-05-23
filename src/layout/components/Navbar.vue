24835dd0036bcb46a94de34493974c2374fd8124
likole <likolef@gmail.com>
2020-05-23 21:55:59 +0800

    暂时移除头像

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -25,7 +25,8 @@
 
       <el-dropdown class="avatar-container right-menu-item hover-effect" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
+<!--          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">-->
+          {{ name }}
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown">
@@ -69,7 +70,8 @@ export default {
     ...mapGetters([
       'sidebar',
       'avatar',
-      'device'
+      'device',
+      'name'
     ])
   },
   methods: {
