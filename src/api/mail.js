b95c0797c8f7461623953edc00c4ed1a7066378c
likole <heo2mfhLPK3Ta0m1>
2020-09-03 23:43:28 +0800

    替换站内信相关接口

--- src/api/mail.js
+++ src/api/mail.js
@@ -1,23 +1,30 @@
-import request from '@/utils/request'
+import newRequest from '../utils/new-request'
 
-export function postEmail(data) {
-  return request({
-    url: '/mail.php',
+export function postMail(data) {
+  return newRequest({
+    url: 'mail',
     method: 'post',
     data
   })
 }
 
-export function fetchEmail(vid) {
-  return request({
-    url: '/mail.php?vid=' + vid,
+export function fetchMail(vid) {
+  return newRequest({
+    url: `mail/${vid}`,
     method: 'get'
   })
 }
 
-export function fetchEmailList() {
-  return request({
-    url: '/mail.php',
+export function fetchMailList() {
+  return newRequest({
+    url: 'mail',
+    method: 'get'
+  })
+}
+
+export function fetchUnreadCount() {
+  return newRequest({
+    url: 'mail/unread',
     method: 'get'
   })
 }
--- src/views/mail/index.vue
+++ src/views/mail/index.vue
@@ -16,8 +16,8 @@
     >
       <el-table-column label="" align="center" width="80px">
         <template slot-scope="scope">
-          <el-tag v-if="scope.row.new_mail">新消息</el-tag>
-          <span v-else>{{ scope.row.mail_id }}</span>
+          <el-tag v-if="scope.row.newMail">新消息</el-tag>
+          <span v-else>{{ scope.row.id }}</span>
         </template>
       </el-table-column>
       <el-table-column label="标题" align="center" min-width="200px">
@@ -27,22 +27,22 @@
       </el-table-column>
       <el-table-column label="用户" align="center" :width="device==='desktop'?'200px':'150px'">
         <template slot-scope="scope">
-          <router-link :to="'/profile/user/'+scope.row.from_user" class="link-type">
-            {{ scope.row.from_user }}
+          <router-link :to="'/profile/user/'+scope.row.fromUser" class="link-type">
+            {{ scope.row.fromUser }}
           </router-link>
         </template>
       </el-table-column>
       <el-table-column label="时间" align="center" :width="device==='desktop'?'180px':'140px'">
         <template slot-scope="scope">
-          {{ scope.row.in_date }}
+          {{ scope.row.inDate }}
         </template>
       </el-table-column>
       <el-table-column align="center" label="操作" width="240">
         <template slot-scope="scope">
-          <el-button type="success" :size="device==='desktop'?'small':'mini'" icon="el-icon-tickets" @click="scope.row.new_mail=false;getDetail(scope.row.mail_id)">
+          <el-button type="success" :size="device==='desktop'?'small':'mini'" icon="el-icon-tickets" @click="scope.row.newMail=false;getDetail(scope.row.id)">
             查看详情
           </el-button>
-          <el-button type="primary" :size="device==='desktop'?'small':'mini'" icon="el-icon-delete" @click="sendEmail(scope.row.from_user)">
+          <el-button type="primary" :size="device==='desktop'?'small':'mini'" icon="el-icon-delete" @click="sendEmail(scope.row.fromUser)">
             回信
           </el-button>
         </template>
@@ -54,7 +54,7 @@
       :width="device==='desktop'?'70%':'95%'"
     >
       <h2>{{ detail.title }}</h2>
-      <p style="color: royalblue">FROM: {{ detail.user }}</p>
+      <p style="color: royalblue">FROM: {{ detail.fromUser }}</p>
       <div>{{ detail.content }}</div>
       <span slot="footer" class="dialog-footer">
         <el-button type="primary" @click="dialogVisible = false">关闭</el-button>
@@ -69,7 +69,7 @@
     >
       <el-form v-if="device==='desktop'" :model="postForm" label-position="left" label-width="70px" style="margin:0 50px;">
         <el-form-item label="对方账号">
-          <el-input v-model="postForm.to_user" />
+          <el-input v-model="postForm.toUser" />
         </el-form-item>
         <el-form-item label="标题">
           <el-input v-model="postForm.title" />
@@ -80,7 +80,7 @@
       </el-form>
       <el-form v-else :model="postForm" style="margin:0 10px;" size="mini">
         <el-form-item label="对方账号">
-          <el-input v-model="postForm.to_user" />
+          <el-input v-model="postForm.toUser" />
         </el-form-item>
         <el-form-item label="标题">
           <el-input v-model="postForm.title" />
@@ -98,7 +98,7 @@
 </template>
 
 <script>
-import { postEmail, fetchEmail, fetchEmailList } from '@/api/mail.js'
+import { postMail, fetchMail, fetchMailList } from '@/api/mail.js'
 import waves from '@/directive/waves' // waves directive
 import { mapState } from 'vuex'
 
@@ -127,25 +127,25 @@ export default {
   methods: {
     getList() {
       this.listLoading = true
-      fetchEmailList().then(response => {
-        this.list = response.data
+      fetchMailList().then(response => {
+        this.list = response.data.list
         this.listLoading = false
       }).catch(() => {
         this.listLoading = false
       })
     },
     getDetail(id) {
-      fetchEmail(id).then(response => {
+      fetchMail(id).then(response => {
         this.detail = response.data
         this.dialogVisible = true
       })
     },
     sendEmail(id) {
-      this.postForm = { to_user: id }
+      this.postForm = { toUser: id }
       this.dialogSendVisible = true
     },
     handleSend() {
-      postEmail(this.postForm).then(() => {
+      postMail(this.postForm).then(() => {
         this.$message({ 'type': 'success', 'message': '发送成功' })
         this.getList()
         this.dialogSendVisible = false
