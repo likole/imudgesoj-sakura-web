489e2a1c19a62f0435b437d27806c02e85a0c104
likole <likolef@gmail.com>
2020-10-20 20:26:05 +0800

    add classroom info in enroll list

--- src/views/imudges/recruit/components/RecruitAdmin.vue
+++ src/views/imudges/recruit/components/RecruitAdmin.vue
@@ -48,7 +48,7 @@
       </el-table-column>
       <el-table-column label="班级" align="center" width="150px">
         <template slot-scope="scope">
-          {{ scope.row.classroom }}
+          {{ scope.row.userInfo.classroomName }}
         </template>
       </el-table-column>
       <el-table-column label="性别" align="center" width="60px">
@@ -142,6 +142,183 @@ export default {
         { label: '游', value: 4 },
         { label: '创', value: 5 },
         { label: '机', value: 6 }
+      ],
+      classroomOptions: [
+        {
+          'label': '计科',
+          'value': 10101,
+          'children': null
+        },
+        {
+          'label': '网工',
+          'value': 10102,
+          'children': null
+        },
+        {
+          'label': '信管',
+          'value': 10103,
+          'children': null
+        },
+        {
+          'label': '电商',
+          'value': 10104,
+          'children': null
+        },
+        {
+          'label': '大数据',
+          'value': 10105,
+          'children': null
+        },
+        {
+          'label': '软一',
+          'value': 10106,
+          'children': null
+        },
+        {
+          'label': '软二',
+          'value': 10107,
+          'children': null
+        },
+        {
+          'label': '电信',
+          'value': 10201,
+          'children': null
+        },
+        {
+          'label': '通信',
+          'value': 10202,
+          'children': null
+        },
+        {
+          'label': '自动化',
+          'value': 10203,
+          'children': null
+        },
+        {
+          'label': '电院其他',
+          'value': 10204,
+          'children': null
+        },
+        {
+          'label': '应用数学',
+          'value': 10301,
+          'children': null
+        },
+        {
+          'label': '计算科学',
+          'value': 10302,
+          'children': null
+        },
+        {
+          'label': '统计学',
+          'value': 10303,
+          'children': null
+        },
+        {
+          'label': '数院其他',
+          'value': 10304,
+          'children': null
+        },
+        {
+          'label': '经济学',
+          'value': 10401,
+          'children': null
+        },
+        {
+          'label': '金融学',
+          'value': 10402,
+          'children': null
+        },
+        {
+          'label': '金融工程',
+          'value': 10403,
+          'children': null
+        },
+        {
+          'label': '管理科学',
+          'value': 10404,
+          'children': null
+        },
+        {
+          'label': '人力资源管理',
+          'value': 10405,
+          'children': null
+        },
+        {
+          'label': '市场营销',
+          'value': 10406,
+          'children': null
+        },
+        {
+          'label': '工商管理',
+          'value': 10407,
+          'children': null
+        },
+        {
+          'label': '会计学',
+          'value': 10408,
+          'children': null
+        },
+        {
+          'label': '财务管理',
+          'value': 10409,
+          'children': null
+        },
+        {
+          'label': '经管院其他',
+          'value': 10410,
+          'children': null
+        },
+        {
+          'label': '新闻学',
+          'value': 10501,
+          'children': null
+        },
+        {
+          'label': '编辑出版学',
+          'value': 10503,
+          'children': null
+        },
+        {
+          'label': '播音主持',
+          'value': 10503,
+          'children': null
+        },
+        {
+          'label': '文新院其他',
+          'value': 10504,
+          'children': null
+        },
+        {
+          'label': '外院',
+          'value': 106,
+          'children': null
+        },
+        {
+          'label': '物院',
+          'value': 107,
+          'children': null
+        },
+        {
+          'label': '化院',
+          'value': 108,
+          'children': null
+        },
+        {
+          'label': '生科院',
+          'value': 109,
+          'children': null
+        },
+        {
+          'label': '生环',
+          'value': 110,
+          'children': null
+        },
+        {
+          'label': '其他',
+          'value': 111,
+          'children': null
+        }
       ]
     }
   },
@@ -154,6 +331,11 @@ export default {
       getRecruitList(this.listQuery).then(response => {
         this.list = response.data.list
         this.total = response.data.total
+        for (let i = 0; i < this.list.length; i++) {
+          let index = this.classroomOptions.findIndex(item => item.value === this.list[i].userInfo.classroom)
+          if(index >=0)
+          this.list[i].userInfo.classroomName = this.classroomOptions[index].label
+        }
         this.listLoading = false
       }).catch(() => {
         this.listLoading = false
